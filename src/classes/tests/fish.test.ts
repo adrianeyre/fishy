c28fd3bbc0c749dff0df162f1d3d60c64b25f416
Adrian Eyre <adrian.eyre@slimmingworld.co.uk>
2019-12-23 08:39:15 +0000

    Test updates

--- src/classes/tests/fish.test.ts
+++ src/classes/tests/fish.test.ts
@@ -1,9 +1,47 @@
+import * as sinon from "sinon";
+
 import Fish from '../fish';
 
 describe('Fish', () => {
-	it('Should create Fish class', () => {
-		const player = new Fish({}, 1, 1, 1);
+	let randomStub: any;
 
-		expect(player.key).toEqual('fish');
+	beforeEach(() => {
+		randomStub = sinon.stub(Math, 'random').returns(0.5);
+	})
+
+	afterEach(() => {
+		randomStub.restore();
+	})
+
+	it('Should create Fish class', () => {
+		const fish = new Fish({}, 1, 1, 1);
+
+		expect(fish.key).toEqual('fish');
+		expect(fish.width).toEqual(12);
+		expect(fish.size).toEqual(6);
+		expect(fish.fishImage).toEqual('fish3-right.gif');
+		expect(fish.x).toEqual(-12);
+		expect(fish.y).toEqual(0);
+	});
+
+	it('Should move the fish', () => {
+		const fish = new Fish({}, 1, 1, 1);
+		fish.move();
+
+		expect(fish.x).toEqual(-9);
+	});
+
+	it('Should eat the player', () => {
+		const fish = new Fish({}, 1, 1, 1);
+		const result = fish.isEatingPlayer(-12, 0);
+
+		expect(result).toEqual(true);
+	});
+
+	it('Should not eat the player', () => {
+		const fish = new Fish({}, 1, 1, 1);
+		const result = fish.isEatingPlayer(-11, 0);
+
+		expect(result).toEqual(true);
 	});
 });
--- src/classes/tests/player.test.ts
+++ src/classes/tests/player.test.ts
@@ -15,4 +15,61 @@ describe('Player', () => {
 		expect(player.x).toEqual(0);
 		expect(player.y).toEqual(0);
 	});
+
+	it('Should bring back the player lives', () => {
+		const player = new Player({});
+
+		expect(player.amountOfLives()).toEqual(5);
+	});
+
+	it('Should reset player size', () => {
+		const player = new Player({});
+		player.resetPlayerSize()
+
+		expect(player.size).toEqual(10);
+		expect(player.height).toEqual(10);
+		expect(player.width).toEqual(20);
+	});
+
+	it('Should bring back the player lives', () => {
+		const player = new Player({});
+
+		expect(player.looseLife()).toEqual(true);
+		player.looseLife();
+		player.looseLife();
+		player.looseLife();
+		expect(player.looseLife()).toEqual(false);
+	});
+
+	it('Should add 10 to score', () => {
+		const player = new Player({});
+		player.addScore(10)
+
+		expect(player.score).toEqual(10);
+	});
+
+	it('Should move player', () => {
+		const player = new Player({});
+		player.move(100, 200)
+
+		expect(player.x).toEqual(90);
+		expect(player.y).toEqual(195);
+		expect(player.direction).toEqual(false);
+	});
+
+	it('Should grow player', () => {
+		const player = new Player({});
+		expect(player.growPlayer()).toEqual(false);
+
+		expect(player.size).toEqual(11);
+		expect(player.height).toEqual(11);
+		expect(player.width).toEqual(22);
+	});
+
+	it('Should grow player to max', () => {
+		const player = new Player({});
+		player.size = 499;
+
+		expect(player.growPlayer()).toEqual(true);
+	});
 });
